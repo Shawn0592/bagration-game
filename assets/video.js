c1bbe815f35802f6a1bc03ce990ecc5d52da1a36
Dmitry Vansovich <40145478+Shawn0592@users.noreply.github.com>
2019-03-22 11:24:09 +0000

    Update video.js

--- assets/video.js
+++ assets/video.js
@@ -1,16 +1,16 @@
-var tag = document.createElement('script');
-		tag.src = 'https://www.youtube.com/player_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var tv,
-		playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
-var vid = [
-			{'videoId': 'EkEHwBt8Kls', 'startSeconds': 1, 'endMiliseconds': 200000, 'suggestedQuality': '360p'}
-		],
-		randomVid = 0,
-    currVid = randomVid;
+//var tag = document.createElement('script');
+//		tag.src = 'https://www.youtube.com/player_api';
+//var firstScriptTag = document.getElementsByTagName('script')[0];
+//		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//var tv,
+//		playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, //disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
+//var vid = [
+//			{'videoId': 'EkEHwBt8Kls', 'startSeconds': 1, 'endMiliseconds': 200000, 'suggestedQuality': //'360p'}
+//		],
+//		randomVid = 0,
+//    currVid = randomVid;
 
-$('.hi em:last-of-type').html(vid.length);
+//$('.hi em:last-of-type').html(vid.length);
 
 function onYouTubePlayerAPIReady(){
   tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
